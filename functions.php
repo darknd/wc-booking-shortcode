dbf8bdcf2e5b4da69d727a568d52fb5ef7ed8764
Francisco Afonso <paco.afonso.perez@gmail.com>
2019-05-12 15:51:50 +0100

    Update functions.php
    Some indent

--- functions.php
+++ functions.php
@@ -5,7 +5,8 @@ function shortcode_list()
 
 	if ($current_user->display_name == 'admin')
 	{
-		$results = $wpdb->get_results("SELECT ID,post_parent FROM wp_posts WHERE post_status = 'paid' AND post_type = 'wc_booking'");
+		$results = $wpdb->get_results("SELECT ID,post_parent FROM wp_posts
+					       WHERE post_status = 'paid' AND post_type = 'wc_booking'");
 		echo "<table>
 		        <thead>
               <tr>
@@ -21,17 +22,33 @@ function shortcode_list()
 		echo "<tr>";
 		foreach ($results as $item)
 		{
-			$personal_data = $wpdb->get_results("SELECT max(CASE WHEN meta_key = '_billing_first_name' AND post_id = $item->post_parent THEN meta_value END) as first_name,	max(CASE WHEN meta_key = '_billing_last_name' AND post_id = $item->post_parent THEN meta_value END) as last_name,max(CASE WHEN meta_key = '_billing_email' AND post_id = $item->post_parent THEN meta_value END) as email,max(CASE WHEN meta_key = '_billing_phone' AND post_id = $item->post_parent THEN meta_value END) as phone FROM wp_postmeta WHERE post_id = $item->post_parent");
+			$personal_data = $wpdb->get_results("
+			SELECT max(CASE WHEN meta_key = '_billing_first_name'
+			AND post_id = $item->post_parent THEN meta_value END) as first_name,
+			max(CASE WHEN meta_key = '_billing_last_name' AND post_id = $item->post_parent THEN meta_value END)
+			as last_name,max(CASE WHEN meta_key = '_billing_email' AND post_id = $item->post_parent THEN
+			meta_value END) as email,max(CASE WHEN meta_key = '_billing_phone' AND
+			post_id = $item->post_parent THEN meta_value END) as phone 
+			FROM wp_postmeta WHERE post_id = $item->post_parent");
+			
 			foreach ($personal_data as $personal)
 			{
 				echo "<td>".$personal->first_name."</td>";
 				echo "<td>".$personal->last_name."</td>";
 				echo "<td>".$personal->email."</td>";
 				echo "<td>".$personal->phone."</td>";
-				$booking_id = $wpdb->get_results("SELECT order_item_id FROM wp_woocommerce_order_itemmeta WHERE meta_value = $item->ID ");
+				$booking_id = $wpdb->get_results("
+				SELECT order_item_id FROM wp_woocommerce_order_itemmeta WHERE meta_value = $item->ID ");
+				
 				foreach ($booking_id as $id)
 				{
-					$day_booking = $wpdb->get_results("SELECT max(CASE WHEN meta_key = '_line_total' AND order_item_id = $id->order_item_id THEN meta_value END) as paid,  max(CASE WHEN meta_key = 'Día de reserva' AND order_item_id = $id->order_item_id THEN meta_value END) as booking FROM wp_woocommerce_order_itemmeta WHERE order_item_id = $id->order_item_id ");
+					$day_booking = $wpdb->get_results("
+					SELECT max(CASE WHEN meta_key = '_line_total' 
+					AND order_item_id = $id->order_item_id THEN meta_value END) as paid,
+					max(CASE WHEN meta_key = 'Día de reserva' AND
+					order_item_id = $id->order_item_id THEN meta_value END) as booking
+					FROM wp_woocommerce_order_itemmeta WHERE order_item_id = $id->order_item_id ");
+					
 					foreach($day_booking as $day)
 					{
 						echo "<td>".$day->booking."</td>";
